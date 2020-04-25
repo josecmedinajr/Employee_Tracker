a0f807b2546564718726a64b8f76d9b89b132ba9
Jose Medina <yanoya10@hotmail.com>
2020-04-24 22:48:49 -0400

    Created schema and seed files and created dbase on mysql workbench

---
+++ db/schema.sql
@@ -0,0 +1,47 @@
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+id INT(10) AUTO_INCREMENT NOT NULL,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY(id)
+);
+
+-- * **department**:
+--   * **id** - INT PRIMARY KEY
+--   * **name** - VARCHAR(30) to hold department name
+
+CREATE TABLE employee (
+ id INT(10) AUTO_INCREMENT NOT NULL,
+ first_name VARCHAR(30) NOT NULL,
+ last_name VARCHAR(30) NOT NULL,
+ role_id INT(10) NOT NULL,
+ manager_id INT(10),
+ PRIMARY KEY(id)
+);
+
+-- * **employee**:
+--   * **id** - INT PRIMARY KEY
+--   * **first_name** - VARCHAR(30) to hold employee first name
+--   * **last_name** - VARCHAR(30) to hold employee last name
+--   * **role_id** - INT to hold reference to role employee has
+--   * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
+
+CREATE TABLE role (
+ id INT (10) AUTO_INCREMENT NOT NULL,
+ title VARCHAR (30) NOT NULL,
+ salary DECIMAL (15) NOT NULL,
+ department_id INT (10) NOT NULL, 
+ PRIMARY KEY (id)
+);
+
+-- * **role**:
+
+--   * **id** - INT PRIMARY KEY
+--   * **title** -  VARCHAR(30) to hold role title
+--   * **salary** -  DECIMAL to hold role salary
+--   * **department_id** -  INT to hold reference to department role belongs to
+
---
+++ db/seed.sql
@@ -0,0 +1,24 @@
+USE employees_db;
+
+INSERT INTO department (name) VALUES ("Technique");
+INSERT INTO department (name) VALUES ("Tours");
+INSERT INTO department (name) VALUES ("Apparel");
+INSERT INTO department (name) VALUES ("Public Relations");
+
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Roger", "Federer", 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Serena", "Williams", 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Rafael", "Nadal", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Maria", "Sharapova", 3, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Stan", "Wawrinka", 4, 1);
+
+INSERT INTO role (title, salary, department_id) VALUES ("Chief Technician", 1500000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Slams CEO", 1800000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Phys Development Master", 1700000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Fashion CEO", 1800000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Chief Financial Officer", 2000000, 4);
+
+
+
+
+
